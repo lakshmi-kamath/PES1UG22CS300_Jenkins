3dd4c270669d171728edf650a0d04a80450d3fc8
lakshmi-kamath <l151lakshmik@gmail.com>
2025-03-13 19:57:35 +0530

    Added a line

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout<<"Automated build is working\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
